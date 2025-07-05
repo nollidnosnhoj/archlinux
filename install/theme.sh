ed9f7377ab728b1a6567c126da2169cde26cf6e4
Dillon Johnson <me@nollidnosnhoj.com>
2025-07-04 22:44:45 -1000

    cursor and icon themes

--- install/theme.sh
+++ install/theme.sh
@@ -5,3 +5,14 @@ sudo pacman -S --noconfirm kvantum-qt5
 sudo pacman -S --noconfirm gnome-themes-extra
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+
+PACKAGES=(
+    bibata-cursor-theme-bin
+    papirus-icon-theme-git
+)
+
+paru -S --needed --noconfirm "${PACKAGES[@]}"
+
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+gsettings set org.gnome.desktop.interface cursor-theme "Bibata-Modern-Ice"
+gsettings set org.gnome.desktop.interface cursor-size 24
